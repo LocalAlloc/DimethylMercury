bb5e40fe3a4bdb0ba13ec92e4c82fc3a48d1514c
LocalAlloc <72191792+LocalAlloc@users.noreply.github.com>
2023-07-30 07:19:06 -0700

    Delete DimethylMercury directory

--- DimethylMercury/MBR/qemudbg.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-nasm MBR.asm -o MBR.bin
-qemu-system-x86_64 -drive file=MBR.bin,format=raw -monitor stdio -m 256 -no-shutdown -no-reboot -L "C:\Program Files\qemu"
--- DimethylMercury/Sound.h
+++
@@ -1,4 +0,0 @@
-#include <Windows.h>
-#include <math.h>
-#pragma comment(lib, "winmm.lib")
-extern VOID WINAPI AudioPayloadThread(VOID);
